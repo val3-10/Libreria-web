d8d302b08eaba9869321c082281c3a19809b3387
val3-10 <danna.quitian@cun.edu.co>
2026-03-29 17:01:54 -0500

    estilos

--- server/scripts/insert.sql
+++ server/scripts/insert.sql
@@ -1,24 +1,23 @@
-INSERT INTO dbo.Libros (Titulo, Autor, Estado, Stock, Precio, CaratulaUrl)
-VALUES
-  (N'Cien años de soledad', N'Gabriel García Márquez', N'disponible', 12, 45000, N'caratula/cien años de soledad.jpg'),
-  (N'El principito', N'Antoine de Saint-Exupéry', N'disponible', 30, 22000,  N'El principito.jfif'),
-  (N'1984', N'George Orwell', N'disponible', 8, 35000, N'caratula/1984jfif.jfif'),
-  (N'El corazón delator', N'Edgar Allan Poe', N'disponible', 18, 35000, N'caratula/El corazon delator jfif. jfif'),
-  (N'El gato negro', N'Edgar Allan Poe', N'disponible', 20, 45000, N'caratula/El gato negro jfif. jfif'),
-  (N'El patito feo', N'Hans Christian Andersen', N'disponible', 28, 35000, N'caratula/El patito feo jfif. jfif'),
-  (N'El resplandor', N'Stephen King', N'disponible', 15, 40000, N'caratula/El resplandor jfif. jfif'),
-  (N'La muerte de la mascara roja', N'Edgar Allan Poe', N'disponible', 28, 35000, N'caratula/La muerte de la mascara roja jfif. jfif'),
-  (N'IT', N'Stephen King', N'disponible', 28, 40000, N'caratula/Libro de IT jfif. jfif'),
-  (N'IT', N'Stephen King', N'disponible', 28, 40000, N'caratula/Libro de IT jfif. jfif'),
-  (N'A dos metros de ti', N'Rachael Lippincott', N'disponible', 28, 52000, N'caratula/A dos metros de tijfif. jfif'),
-  (N'Berserk', N'Kentaro Miura', N'disponible', 28, 37900, N'caratula/Berserk. jfif'),
-  (N'Cementerios de animales', N'Stephen King', N'disponible', 28, 59000, N'caratula/Cementerios de animales. jfif'),
-  (N'De la tierra a la luna', N'Julio Verne', N'disponible', 28, 40000, N'caratula/De la tierra a la luna. jfif'),
-  (N'El visitante', N'Stephen King', N'disponible', 28, 55000, N'caratula/El visitante. jfif'),
-  (N'Hansel y Gretel', N'Hermanos Grimm', N'disponible', 28, 40000, N'caratula/gretel y hanseljfif. jfif'),
-  (N'Cómo entrenar a tu dragón', N'Cressida Cowell', N'disponible', 28, 45000, N'caratula/How to Train Your Dragon. jfif'),
-  (N'Misery', N'Stephen King', N'disponible', 28, 50000, N'caratula/Misery. jfif'),
-  (N'Parque Jurásico', N'Michael Crichton', N'disponible', 28, 60000, N'caratula/Parque jurásico. jfif'),
-  (N'Viaje al centro de la tierra', N'Julio Verne', N'disponible', 28, 35000, N'caratula/Viaje al centro de la tierra. jfif');
+  INSERT INTO dbo.Libros (Titulo, Autor, Estado, Stock, Precio, CaratulaUrl)
+  VALUES
+    (N'Cien años de soledad', N'Gabriel García Márquez', N'disponible', 12, 45000, N'caratulas/cien años de soledad.jpg'),
+    (N'El principito', N'Antoine de Saint-Exupéry', N'disponible', 30, 22000,  N'caratulas/El principito.jpg'),
+    (N'1984', N'George Orwell', N'disponible', 8, 35000, N'caratulas/1984.jpg'),
+    (N'El corazón delator', N'Edgar Allan Poe', N'disponible', 18, 35000, N'caratulas/El corazon delator.jpg'),
+    (N'El gato negro', N'Edgar Allan Poe', N'disponible', 20, 45000, N'caratulas/El gato negro.jpg'),
+    (N'El patito feo', N'Hans Christian Andersen', N'disponible', 28, 35000, N'caratulas/El patito feo.jpg'),
+    (N'El resplandor', N'Stephen King', N'disponible', 15, 40000, N'caratulas/El resplandor.jpg'),
+    (N'La muerte de la mascara roja', N'Edgar Allan Poe', N'disponible', 28, 35000, N'caratulas/La muerte de la mascara roja.jpg'),
+    (N'IT', N'Stephen King', N'disponible', 28, 40000, N'caratulas/Libro de IT.jpg'),
+    (N'A dos metros de ti', N'Rachael Lippincott', N'disponible', 28, 52000, N'caratulas/A dos metros de ti.jpg'),
+    (N'Berserk', N'Kentaro Miura', N'disponible', 28, 37900, N'caratulas/Berserk.jpg'),
+    (N'Cementerio de animales', N'Stephen King', N'disponible', 28, 59000, N'caratulas/cementerio de animales.jpg'),
+    (N'De la tierra a la luna', N'Julio Verne', N'disponible', 28, 40000, N'caratulas/De la tierra a la luna.jpg'),
+    (N'El visitante', N'Stephen King', N'disponible', 28, 55000, N'caratulas/El visitante.jpg'),
+    (N'Hansel y Gretel', N'Hermanos Grimm', N'disponible', 28, 40000, N'caratulas/gretel y hansel.jpg'),
+    (N'Cómo entrenar a tu dragón', N'Cressida Cowell', N'disponible', 28, 45000, N'caratulas/How to Train Your Dragon.jpg'),
+    (N'Misery', N'Stephen King', N'disponible', 28, 50000, N'caratulas/Misery.jpg'),
+    (N'Parque Jurásico', N'Michael Crichton', N'disponible', 28, 60000, N'caratulas/Parque jurásico.jpg'),
+    (N'Viaje al centro de la tierra', N'Julio Verne', N'disponible', 28, 35000, N'caratulas/Viaje al centro de la tierra.jpg');
 
-GO
+  GO
